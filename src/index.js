3dadc469c7092b09b83cef43eb870b4261961c91
strtw <souporstu@gmail.com>
2020-06-04 13:36:02 -0700

    App loads product list to page
    - Added table-maker module to create product table using template
      literals
    - Update page styles to reflect BEM CSS methodology

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import {productList} from './product-dummy-list.json'
+import productTableMaker from './modules/table-maker'
+let products = productList[0].productFacetInfoList //get product array
 
-let products = productList[0].productFacetInfoList
-console.log(products)
+const main = document.getElementById('main') //get reference to main html element
 
 var ProductPage = (function productPageCreator(productList){
     return
 })(products)
+
+main.innerHTML = productTableMaker(products) //insert product table into main element
